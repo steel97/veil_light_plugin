63da8dbef5fd2c87dc06a9dc0ea5fea838ee7fcf
Ivan Yv <steel-97@mail.ru>
2023-10-02 21:46:06 +0300

    Updated build scripts

--- ios/Classes/veil_light_plugin.c
+++ ios/Classes/veil_light_plugin.c
@@ -1,3 +1,1 @@
-// Relative import to be able to reuse the C sources.
-// See the comment in ../{projectName}}.podspec for more information.
-#include "../../src/veil_light_plugin.c"
+// This is an empty file to force CocoaPods to create a framework.
